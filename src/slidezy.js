5f491eff3821e12d42546b46de647a7e50de24d6
John Tran <phuocthanh03062004@gmail.com>
2026-02-17 17:14:37 +0700

    Xây dựng dự án Slidezy

---
+++ src/slidezy.js
@@ -0,0 +1,52 @@
+function Slidezy(selector, options = {}) {
+    this.container = document.querySelector(selector);
+
+    if (!this.container) {
+        console.error(`Slidezy: Container "${selector}" not found!`);
+        return;
+    }
+
+    this.opt = Object.assign({}, options);
+    this.slides = Array.from(this.container.children);
+
+    this._init();
+}
+
+Slidezy.prototype._init = function () {
+    this.container.classList.add("slidezy-wrapper");
+
+    this._createTrack();
+    this._createNavigation();
+};
+
+Slidezy.prototype._createTrack = function () {
+    this.track = document.createElement("div");
+    this.track.className = "slidezy-track";
+
+    this.slides.forEach((slide) => {
+        slide.classList.add("slidezy-slide");
+        this.track.appendChild(slide);
+    });
+
+    this.container.append(this.track);
+};
+
+Slidezy.prototype._createNavigation = function () {
+    this.btnPrev = document.createElement("button");
+    this.btnNext = document.createElement("button");
+
+    this.btnPrev.className = "slidezy-prev";
+    this.btnNext.className = "slidezy-next";
+
+    this.btnPrev.innerText = "<";
+    this.btnNext.innerText = ">";
+
+    this.container.append(this.btnPrev, this.btnNext);
+
+    this.btnPrev.onclick = () => this.moveSlide(-1);
+    this.btnNext.onclick = () => this.moveSlide(1);
+};
+
+Slidezy.prototype.moveSlide = function (step) {
+    console.log(step);
+};
